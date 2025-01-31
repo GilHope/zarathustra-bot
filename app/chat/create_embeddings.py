c40e107d35a2e97967f32ffd304363c14d337ece
Gil <ghopecs@gmail.com>
2025-01-31 09:26:29 -0500

    add docs to vector

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -1,5 +1,6 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from app.chat.vector_stores.pinecone import vector_store
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -9,3 +10,5 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     
     loader = PyPDFLoader(pdf_path)
     docs = loader.load_and_split(text_splitter)
+
+    vector_store.add_documents(docs)
