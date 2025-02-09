acced87772d8a65e273895b228eac6df829220c7
Gil <ghopecs@gmail.com>
2025-02-09 11:15:28 -0500

    typo

--- app/chat/memories/sql_memory.py
+++ app/chat/memories/sql_memory.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from langchain.memory import ConcersationBuferMemory
 from langchain.schema import BaseChatMessageHistory
 
-from app.web.apt import (
+from app.web.api import (
     get_messages_by_conversation_id,
     add_message_to_conversation
 )
