eaf21de42665f4c73cf325df2c101a37a0cc0a4b
Gil <ghopecs@gmail.com>
2025-02-09 11:22:33 -0500

    def messages and add_message

--- app/chat/memories/sql_memory.py
+++ app/chat/memories/sql_memory.py
@@ -9,3 +9,17 @@ from app.web.api import (
 
 class SqlMessageHistory(BaseChatMessageHistory, BaseModel):
     conversation_id: str
+
+    @property
+    def messages(self):
+        return get_message_by_conversation_id(self.conversation_id)
+    
+    def add_message(self, message):
+        return add_message_to_conversation(
+            conversation_id=self.conversation_id,
+            role=message.type,
+            content=message.content
+        )
+    
+    def clear(self):
+        pass
